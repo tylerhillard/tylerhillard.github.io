bd90e606ec2d203f83c64a7fa391be4d09f0180b
tylerhillard5 <tyhillard@gmail.com>
2023-03-10 13:22:46 -0500

    finished featured work section

---
+++ script.js
@@ -0,0 +1,19 @@
+//Source Code Button
+function showPageSource() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", document.location, true);
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState == 4 && xhr.status == 200) {
+		var sourceWindow = window.open("", "", "width=800,height=600");
+		sourceWindow.document.write("<pre>" + xhr.responseText.replace(/[<>&]/g, function(c) {
+		  return {'<':'&lt;','>':'&gt;','&':'&amp;'}[c];
+		}) + "</pre>");
+	  }
+	};
+	xhr.send();
+  }
+
+  
+function openMarketing() {
+	window.open("projects/Final.pptx", "_blank");
+}
